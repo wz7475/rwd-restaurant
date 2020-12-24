2dde6772396741e47532c58b3ae755ee7240296e
Wojtek Zarzecki <wojtek.zarzecki2002@gmail.com>
2020-12-24 14:39:18 +0100

    add python script to rename phothos

---
+++ rename.py
@@ -0,0 +1,22 @@
+import os
+import shutil
+
+print("Przeciągnij folder lub wpisz ścieżkę:\n")
+path = input()
+
+#path = "c:\\Users\\wojte\\Desktop\\test"
+#path = "/mnt/c/users/wojte/Desktop/test"
+print(path)
+os.chdir(path)
+
+counter = 1
+for i in os.listdir():
+    if os.path.isfile(i):
+        extension = os.path.splitext(i)[1]
+        if counter < 10:
+            new_name = "0" + str(counter) + extension
+        else:
+            new_name = str(counter) + extension
+        os.rename(i, os.path.join(path, new_name))
+        counter += 1;
+        
