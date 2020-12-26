d2bea3b3e36f86a6f49e3fc37d74ba03f9ef76d0
Wojtek Zarzecki <wojtek.zarzecki2002@gmail.com>
2020-12-26 19:22:39 +0100

    flexbox gallet 1

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
 const generate = () =>{
     let gallery1= "";
-    for (let i =1; i<=19; i++){
-        let pic_name = i + ".jpg";
-        if (i<10)
+    for (let i =0; i<17; i++){
+        let pic_name = `${i + 1}.jpg`;
+        if (i<10-1)
             pic_name = "0" + pic_name;
         pic_name = "img/" + pic_name;
-        gallery1 += `<div class="pic"><img src="${pic_name}" alt="taco"></div>\n`; 
+        if (((i - 1) % 4 == 0 || i == 0) && i != 1){
+            gallery1 += '<div class="column">\n';
+        }
+        /* gallery1 += `<div class="pic"><img src="${pic_name}" alt="taco"></div>\n`; */ 
+        gallery1 += `<img src="${pic_name}" alt="taco">\n`;
+        if (i % 4 == 0 && i != 0){
+            gallery1 += '</div>\n';
+        }
     }
     document.getElementById("gallery1").innerHTML = gallery1;
 }
